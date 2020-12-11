0bdfef7114cdf68b13f7cc7c4468c0388beeb8d0
George Zaragoza <63305929+georgeomics@users.noreply.github.com>
2020-12-11 14:10:53 -0500

    Add files via upload

---
+++ NCBI Download.sh
@@ -0,0 +1,28 @@
+# Code used to download fasta files for study:
+
+# Snow Leopard
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_010638.1&rettype=fasta" -O NC_010638.1_pathera-uncia.fa
+
+# Leopard
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_010641.1&rettype=fasta" -O NC_010641.1_pathera-pardus.fa
+
+# Jaguar
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_022842.1&rettype=fasta" -O NC_022842.1_pathera-onca.fa
+
+# African Lion
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_028302.1&rettype=fasta" -O NC_028302.1_pathera-leo.fa
+
+# South China Tiger
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_014770.1&rettype=fasta" -O NC_014770.1_pathera-tigris-amoyensis.fa
+
+# Siberian Tiger
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_010642.1&rettype=fasta" -O NC_010642.1_pathera-tigris-altaica.fa
+
+# Vicuna
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_013558.1&rettype=fasta" -O NC_013588.1_vicugna-vicugna.fa
+
+# Himalayan Tahr
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_020628.1&rettype=fasta" -O NC_020628.1_hemitragus-jemlahicus.fa
+
+# Tibetan Plateau Fox
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_027935.1&rettype=fasta" -O NC_027935.1_vulpes-ferrilata.fa
